ccf8772e1f7458d963760d2e1e0244cf56b9a741
anas <astalaat3@gmail.com>
2023-10-07 13:05:56 +0300

    ..

--- client/app/Components/LandingPage.tsx
+++ client/app/Components/LandingPage.tsx
@@ -1,18 +1,17 @@
-'use client'
 import Image from "next/image";
 import Landing from '../../assets/landing.webp';
 import Link from "next/link";
 
-export default function LandingPage(){
-    return(
-        <div className='w-full h-full p-6 max-md:p-0 relative mt-32'>
-            <Image
-            className='w-full h-full rounded-xl mix-blend-hard-light'
-                src={Landing}
-                alt="Cover"
-             />
-            <h2 className='text-white text-4xl max-md:text-lg max-md:left-8 max-md:top-2/4 max-md:mt-4 max-lg:mt-0 font-bold absolute top-3/4 left-20 mt-14'>AS Blog | Create your posts now!</h2>
-            <Link href='/Auth/Register' className='text-yellow-500 duration-200 hover:text-black underline text-4xl max-md:text-sm max-md:left-8 max-md:top-2/4 absolute top-3/4 left-20 mt-24 max-md:mt-10 max-lg:mt-10'>Join us</Link>
-        </div>
-    )
+export default function LandingPage() {
+  return (
+    <div className='w-full h-full p-6 max-md:p-0 relative mt-32'>
+      <Image
+        className='w-full h-full rounded-xl mix-blend-hard-light'
+        src={Landing}
+        alt="Cover"
+      />
+      <h2 className='text-white text-4xl max-md:text-lg max-md:left-8 max-md:top-2/4 max-md:mt-4 max-lg:mt-0 font-bold absolute top-3/4 left-20 mt-14'>AS Blog | Create your posts now!</h2>
+      <Link href='/Auth/Register' className='text-yellow-500 duration-200 hover:text-black underline text-4xl max-md:text-sm max-md:left-8 max-md:top-2/4 absolute top-3/4 left-20 mt-24 max-md:mt-10 max-lg:mt-10'>Join us</Link>
+    </div>
+  );
 }
--- client/app/Posts/EditPost/[postId]/page.tsx
+++ client/app/Posts/EditPost/[postId]/page.tsx
@@ -1,118 +1,123 @@
-'use client'
 import React, { useEffect, useRef, useState } from 'react';
 import JoditEditor from 'jodit-react';
 import { Input, TextField } from '@mui/material';
 import axios from 'axios';
 import { POST, UPDATE_POST } from '@/utils/apis';
-import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/router';
 
-export default function EditPost({params}: any){
-    const [content, setContent] = useState<string>('');
-    const editor = useRef(null);
-    const [selectedImg, setSelectedImg] = useState<File | undefined>(undefined);
-    const [error, setError] = useState<string | null>(null);
-    const [success, setSuccess] = useState<string | null>(null);
-    const router = useRouter();
-    const postId = params.postId;
-    
-    const [form, setForm] = useState({
-        title: '',
-        summary: ''
-    })
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const res = await axios.get(POST + postId);
-                const postData = res.data.data.foundPost;
-                setForm({title: postData.title, summary: postData.summary}),
-                setContent(postData.content)
-            }catch(err){
-                console.log(err)
-            }
-        }
-        fetchData();
-    }, [postId])
+export default function EditPost({ params }: any) {
+  const [content, setContent] = useState<string>('');
+  const editor = useRef(null);
+  const [selectedImg, setSelectedImg] = useState<File | undefined>(undefined);
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+  const router = useRouter();
+  const postId = params.postId;
 
+  const [form, setForm] = useState({
+    title: '',
+    summary: '',
+  });
 
-    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = event.target;
-        setForm({...form, [name]: value})
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(POST + postId);
+        const postData = res.data.data.foundPost;
+        setForm({ title: postData.title, summary: postData.summary });
+        setContent(postData.content);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleImgChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setSelectedImg(file);
+  };
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    const data = new FormData();
+    if (selectedImg) {
+      data.append('thumbnail', selectedImg);
     }
+    data.append('title', form.title);
+    data.append('summary', form.summary);
+    data.append('content', content);
 
-    const handleImgChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        setSelectedImg(file);
-      };
-      
-    const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
+    try {
+      await axios.put(UPDATE_POST + postId, data);
 
-        const data = new FormData();
-        if (selectedImg) {
-            data.append('thumbnail', selectedImg);
-        }
-        data.append('title', form.title)
-        data.append('summary', form.summary)
-        data.append('content', content)
-
-        try{
-            await axios.put(UPDATE_POST + postId, data);
-
-            setError(null);
-            setSuccess('Post has been updated!')
-            setTimeout(() => {
-                router.push('/Posts/MyPosts');
-                setSuccess(null)
-            }, 2000)
-
-        }catch(err: any){
-            setError(err.response?.data.message)
-            if(err.response.status = 500){
-                setError('post thumbnail is required!');
-            }else{
-                setError(err.response?.data.message);
-            }
-        }
+      setError(null);
+      setSuccess('Post has been updated!');
+      setTimeout(() => {
+        router.push('/Posts/MyPosts');
+        setSuccess(null);
+      }, 2000);
+    } catch (err: any) {
+      console.error(err);
+      if (err.response.status === 500) {
+        setError('Post thumbnail is required!');
+      } else {
+        setError(err.response?.data.message);
+      }
     }
+  };
 
-    return(
-        <div className='flex flex-col gap-5 items-center justify-center  mt-32 mb-28'>
-            <div className='flex flex-col gap-10 w-[500px] min-h-[600px] bg-slate-100 rounded-md p-5  max-md:w-5/6'>
-            <TextField
-            label="title"
-            name='title'
-            variant="outlined"
-            fullWidth
-            onChange={handleFormChange}
-            value={form.title}
-            />
-            <TextField
-            label="summary"
-            name='summary'
-            variant="outlined"
-            fullWidth
-            onChange={handleFormChange}
-            value={form.summary}
-            />
-            <Input
-            id="file-input"
-            type="file"
-            onChange={handleImgChange}
-            inputProps={{ accept: 'image/*' }}
-            />
-                <JoditEditor
-                    className='w-full h-full text-black bg-black'
-                    ref={editor}
-                    value={content}
-                    onBlur={(newContent) => setContent(newContent)}
-                    onChange={(newContent) => setContent(newContent)}
-                    />
-                    <button onClick={handleSubmit} className='w-[150px] h-[40px] bg-blue-500 text-white flex justify-center items-center rounded-md'>
-                    submit</button>
-                    {error && <p className='text-red-500 bg-white p-5 rounded-md text-md'>{error}</p>}
-                    {success && <p className='text-green-500 bg-white p-5 rounded-md text-md'>{success}</p>}
-                </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center mt-32 mb-28">
+      <div className="flex flex-col gap-10 w-[500px] min-h-[600px] bg-slate-100 rounded-md p-5 max-md:w-5/6">
+        <TextField
+          label="title"
+          name="title"
+          variant="outlined"
+          fullWidth
+          onChange={handleFormChange}
+          value={form.title}
+        />
+        <TextField
+          label="summary"
+          name="summary"
+          variant="outlined"
+          fullWidth
+          onChange={handleFormChange}
+          value={form.summary}
+        />
+        <Input
+          id="file-input"
+          type="file"
+          onChange={handleImgChange}
+          inputProps={{ accept: 'image/*' }}
+        />
+        <JoditEditor
+          className="w-full h-full text-black bg-black"
+          ref={editor}
+          value={content}
+          onBlur={(newContent) => setContent(newContent)}
+          onChange={(newContent) => setContent(newContent)}
+        />
+        <button
+          onClick={handleSubmit}
+          className="w-[150px] h-[40px] bg-blue-500 text-white flex justify-center items-center rounded-md"
+        >
+          submit
+        </button>
+        {error && (
+          <p className="text-red-500 bg-white p-5 rounded-md text-md">{error}</p>
+        )}
+        {success && (
+          <p className="text-green-500 bg-white p-5 rounded-md text-md">{success}</p>
+        )}
+      </div>
+    </div>
+  );
 }
