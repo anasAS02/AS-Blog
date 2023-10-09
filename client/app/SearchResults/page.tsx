d7d018afac2077cc900f3fb26ba78e27537756bb
anas <astalaat3@gmail.com>
2023-10-09 11:24:44 +0300

    .

--- client/app/SearchResults/page.tsx
+++ client/app/SearchResults/page.tsx
@@ -21,7 +21,7 @@ const SearchResult = () => {
   }, [keyword])
   
   return (
-    <div className='w-full flex flex-col gap-10 mt-36 p-5 mb-64'>
+    <div className='w-full flex flex-col gap-10 mt-36 p-5 mb-72'>
       <h2 className='text-white text-4xl font-bold'>Search Results</h2>
       {posts? 
       <div className='grid grid-cols-1 gap-10'>
