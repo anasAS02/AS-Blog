423e8196f7df4903200eb747051272d1b3f7f596
anas <astalaat3@gmail.com>
2023-10-09 10:26:56 +0300

    .

--- client/app/Components/LandingPage.tsx
+++ client/app/Components/LandingPage.tsx
@@ -12,7 +12,7 @@ export default function LandingPage(){
                 alt="Cover"
              />
             <h2 className='text-white text-4xl max-md:text-xl max-md:left-8 max-md:top-2/4 max-md:mt-4 max-lg:mt-0 font-bold absolute top-3/4 left-20 mt-14'>AS Blog | Create your posts now!</h2>
-            <Link href='/Auth/Register' className='text-yellow-500 duration-200 hover:text-black underline text-4xl max-md:text-xl max-md:left-8 max-md:top-2/4 absolute top-3/4 left-20 mt-24 max-md:mt-10 max-lg:mt-10'>Join us</Link>
+            <Link href='/Auth/Register' className='text-yellow-500 duration-200 hover:text-black underline text-4xl max-md:text-lg max-md:left-8 max-md:top-2/4 absolute top-3/4 left-20 mt-24 max-md:mt-16 max-lg:mt-10'>Join us</Link>
         </div>
     )
 }
